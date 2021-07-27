10fb972ae5b6726135952f8030f43a81beffec4a
Eric Saldivar <Eric@samlas-mbp.lan>
2021-07-27 11:02:00 -0500

    removed palindrome solution

--- Strings/Easy/stringsE.js
+++ Strings/Easy/stringsE.js
@@ -1,8 +1,5 @@
 const isPalindrome = (string) => {
 	// Write your code here.
-	if(string.length === 1) return true;
-	const revStr = string.split('').reverse().join('');
-	return string === revStr;
 }
   
 // Do not edit the line below.
