3ad91cedc90b3993d1fb4724a50a7d28f745f70b
Eric Saldivar <Eric@samlas-mbp.lan>
2021-07-30 22:39:12 -0500

    added dup nums to arrays

--- Arrays/Easy/arraysE.js
+++ Arrays/Easy/arraysE.js
@@ -17,5 +17,25 @@ const twoSum = (arr, target) => {
   // Write your code here.
 }
 
+/*
+You have an unsorted array of n + 1 numbers, with the numbers from 1 to n.
+One number is duplicated. Find it.
+ex: [1,5,4,3,6,2,4,7] should return 4
+*/
 
-module.exports = { twoSum };
+const duplicateNumber = array => {
+  // Write your code here.
+};
+
+
+/* 
+You have an unsorted array of n + 1 numbers, with the range of k to k + n - 1, with an extra number that is a duplicate.
+(k is not necessarily 1) Find the duplicate. Do this in O(n) time and O(1) space complexity.
+ex: [3, 4, 7, 6, 8, 5, 6] should return 6
+*/
+
+const duplicateNumberAdvanced = array => {
+  // Write your code here.
+};
+
+module.exports = {twoSum, duplicateNumber, duplicateNumberAdvanced };
--- __test__/arrays.test.js
+++ __test__/arrays.test.js
@@ -1,4 +1,4 @@
-const { twoSum} = require('../Arrays/Easy/arraysE');
+const {twoSum, duplicateNumber, duplicateNumberAdvanced} = require('../Arrays/Easy/arraysE');
 const {threeSum} = require('../Arrays/Medium/arraysM');
 describe('twoSum test', () => {
   let arr;
@@ -65,3 +65,39 @@ xdescribe('threeSum test', () => {
   });
 
 });
+
+describe('duplicateNumber test', () => {
+	let arr;
+  
+	it('should return duplicate number in a sorted array', () => {
+	  arr = [1,2,3,4,5,6,7,7,8,9,10,11,12,13,14,15];
+	  expect(duplicateNumber(arr)).toBe(7);
+	})
+  
+	it('should return duplicate number in an unsorted array', () => {
+	  arr = [12,14,3,7,15,1,11,9,10,3,5,2,4,6,13,8];
+	  expect(duplicateNumber(arr)).toBe(3);
+	})
+  })
+  
+  xdescribe('duplicateNumberAdvanced test', () => {
+	let arr;
+  
+	it('should return duplicate number in an unsorted array', () => {
+	  arr = [];
+	  for (let i = 42; i < 142; i++){
+		arr.push(i);
+	  }
+	  arr.push(72)
+	  expect(duplicateNumberAdvanced(arr)).toBe(72);
+	})
+  
+	it('should return duplicate number in a sorted array', () => {
+	  arr = [];
+	  for (let i = 73; i < 211; i++){
+		if(i === 111) arr.push(i);
+		arr.push(i);
+	  }
+	  expect(duplicateNumberAdvanced(arr)).toBe(111);
+	})
+  })
